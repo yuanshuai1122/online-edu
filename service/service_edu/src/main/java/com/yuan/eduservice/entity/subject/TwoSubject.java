792338340a09b560852f1a59478ea0601e2490a8
tommy <18232336174@163.com>
2022-02-07 14:37:54 +0800

    课程分类添加

---
+++ service/service_edu/src/main/java/com/yuan/eduservice/entity/subject/TwoSubject.java
@@ -0,0 +1,16 @@
+package com.yuan.eduservice.entity.subject;
+
+import lombok.Data;
+
+/**
+ * @author: yyss
+ * @create: 2022-02-07 13:03
+ **/
+// 二级分类
+@Data
+public class TwoSubject {
+
+    private String id;
+
+    private String title;
+}
